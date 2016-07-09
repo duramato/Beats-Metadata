f9791b4b19b2cf304e322db0a40c95ad4632d5a8
supergonkas <matigonkas@outlook.com>
2016-07-09 21:44:22 +0100

    Update service for covers and music

--- screenshooter.py
+++ screenshooter.py
@@ -1,11 +1,21 @@
 import time
+import os
+import logging as logger
 from selenium import webdriver
 starttime=time.time()
+logger.basicConfig(filename='screenshooter.log',level=logger.DEBUG)
 url = 'http://applemusic.tumblr.com/beats1'
 while True:
-    driver = webdriver.PhantomJS()
-    driver.set_window_size(1920, 1080)
-    driver.get(url)
-    driver.save_screenshot('page.jpg')
-    print('Screenie taken @ ' + str(time.time()) )
-    time.sleep(60.0 - ((time.time() - starttime) % 60.0))
+    #driver = webdriver.PhantomJS()
+    #driver.set_window_size(1920, 1080)
+    #driver.get(url)
+    #driver.save_screenshot('page.jpg')
+    try:
+        print('hey')
+    except Exception:
+        logger.info('No File exsits')
+    logger.info('screenie init')
+    os.system("./webshots --width 1080 --height 1920 https://twitter.com/radio_scrobble")
+    #print('Screenie taken @ ' + str(time.time()) )
+    logger.info('Screenie taken')
+    time.sleep(5.0 - ((time.time() - starttime) % 5.0))
