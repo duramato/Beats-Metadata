ed90b2298d238a408d1f2c819870e8814de91f9b
supergonkas <matigonkas@outlook.com>
2016-07-10 17:53:32 +0100

    Hacky fix for "IOError: cannot identify image file 'twitter.com-radio_scrobble.1080x1920.png'"

--- server.py
+++ server.py
@@ -25,7 +25,7 @@ class SlowHandler(BaseHTTPRequestHandler):
     def do_GET(s):
         """Respond to a GET request."""
         if s.path.startswith('/audio/wat/showimg.jpg'):
-            server = parse_qs(urlparse(s.path).query)
+            #server = parse_qs(urlparse(s.path).query)
             img = Image.open("page.jpg")
             left = 360
             top = 810
@@ -41,7 +41,7 @@ class SlowHandler(BaseHTTPRequestHandler):
             s.wfile.write(f.read())
             f.close()
         elif s.path.startswith('/audio/wat/show.jpg'):
-            server = parse_qs(urlparse(s.path).query)
+            #server = parse_qs(urlparse(s.path).query)
             img = Image.open("page.jpg")
             left = 460
             top = 830
@@ -57,45 +57,61 @@ class SlowHandler(BaseHTTPRequestHandler):
             s.wfile.write(f.read())
             f.close()
         elif s.path.startswith('/audio/wat/art.jpg'):
-            server = parse_qs(urlparse(s.path).query)
-            img = Image.open("twitter.com-radio_scrobble.1080x1920.png")
-            left = 470
-            top = 580
-            width = 500
-            height = 500
-            box = (left, top, left+width, top+height)
-            img4 = img.crop(box)
-            img4.save("cover.jpg")
-            f=open("cover.jpg", 'rb')
-            s.send_response(200)
-            s.send_header('Content-type',        'image/jpg')
-            s.end_headers()
-            s.wfile.write(f.read())
-            f.close()
+            #server = parse_qs(urlparse(s.path).query)
+            try:
+                img = Image.open("twitter.com-radio_scrobble.1080x1920.png")
+                left = 470
+                top = 580
+                width = 500
+                height = 500
+                box = (left, top, left+width, top+height)
+                img4 = img.crop(box)
+                img4.save("cover.jpg")
+                f=open("cover.jpg", 'rb')
+                s.send_response(200)
+                s.send_header('Content-type',        'image/jpg')
+                s.end_headers()
+                s.wfile.write(f.read())
+                f.close()
+            except IOError:
+                f=open("cover.jpg", 'rb')
+                s.send_response(200)
+                s.send_header('Content-type',        'image/jpg')
+                s.end_headers()
+                s.wfile.write(f.read())
+                f.close()
         elif s.path.startswith('/audio/wat/nowz.jpg'):
-            server = parse_qs(urlparse(s.path).query)
-            img = Image.open("twitter.com-radio_scrobble.1080x1920.png")
-            left = 460
-            top = 470
-            width = 520
-            height = 100
-            box = (left, top, left+width, top+height)
-            img4 = img.crop(box)
-            img4.save("now1.jpg")
+            #server = parse_qs(urlparse(s.path).query)
+            try:
+                img = Image.open("twitter.com-radio_scrobble.1080x1920.png")
+                left = 460
+                top = 470
+                width = 520
+                height = 100
+                box = (left, top, left+width, top+height)
+                img4 = img.crop(box)
+                #img4.save("now1.jpg")
             
-            basewidth = 375
-            img = Image.open('now1.jpg')
-            wpercent = (basewidth/float(img.size[0]))
-            hsize = int((float(img.size[1])*float(wpercent)))
-            img = img.resize((basewidth,hsize), PIL.Image.ANTIALIAS)
-            img.save('now.jpg')
+                basewidth = 375
+                img = img4 #Image.open('now1.jpg')
+                wpercent = (basewidth/float(img.size[0]))
+                hsize = int((float(img.size[1])*float(wpercent)))
+                img = img.resize((basewidth,hsize), PIL.Image.ANTIALIAS)
+                img.save('now.jpg')
             
-            f=open("now.jpg", 'rb')
-            s.send_response(200)
-            s.send_header('Content-type',        'image/jpg')
-            s.end_headers()
-            s.wfile.write(f.read())
-            f.close()
+                f=open("now.jpg", 'rb')
+                s.send_response(200)
+                s.send_header('Content-type',        'image/jpg')
+                s.end_headers()
+                s.wfile.write(f.read())
+                f.close()
+            except IOError:
+                f=open("now.jpg", 'rb')
+                s.send_response(200)
+                s.send_header('Content-type',        'image/jpg')
+                s.end_headers()
+                s.wfile.write(f.read())
+                f.close()
         else:
             f=open("page.jpg", 'rb')
             s.send_response(404)
