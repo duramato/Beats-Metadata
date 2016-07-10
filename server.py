154e95c3a66ae67f57000c9fc506e0a1f6d7cc62
supergonkas <matigonkas@outlook.com>
2016-07-10 19:18:57 +0100

    Update server.py

--- server.py
+++ server.py
@@ -105,7 +105,7 @@ class SlowHandler(BaseHTTPRequestHandler):
                 for y in xrange(img.size[1]):
                    for x in xrange(img.size[0]):
                         if pixdata[x, y] == (255, 255, 255, 255):
-                            pixdata[x, y] = (0, 0, 0, 255)
+                            pixdata[x, y] = (0, 0, 0, 0)
             
                 img4 = img
             
