23a3a4674814dee4b1e3670e885e3d570111ba42
supergonkas <matigonkas@outlook.com>
2016-07-10 19:01:09 +0100

    Print

--- server.py
+++ server.py
@@ -101,9 +101,14 @@ class SlowHandler(BaseHTTPRequestHandler):
                 data = np.array(im)   # "data" is a height x width x 4 numpy array
                 red, green, blue, alpha = data.T # Temporarily unpack the bands for readability
 
+                print(red)
+                print(green)
+                print(blue)
+                print(alpha)
+
                 # Replace white with red... (leaves alpha values alone...)
-                white_areas = (red == 255) & (blue == 255) & (green == 255) & (alpha == 0)
-                data[..., :-1][white_areas.T] = (255, 0, 0, 0) # Transpose back needed
+                white_areas = (red == 255) & (blue == 255) & (green == 255)
+                data[..., :-1][white_areas.T] = (255, 0, 0) # Transpose back needed
 
                 im2 = Image.fromarray(data)
             
