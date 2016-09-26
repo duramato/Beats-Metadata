11e2aff6f4724dec8ec4317454fc951b30ea7e31
supergonkas <matigonkas@outlook.com>
2016-09-26 16:56:04 +0100

    Complete rewritte, make use of classes the exist for a reason.. Make code more efficient, i think

--- server.py
+++ server.py
@@ -6,10 +6,100 @@ from urlparse import urlparse, parse_qs
 from selenium import webdriver
 import numpy as np
 import PIL
+import re
 from PIL import Image
 from PIL import ImageFile
+import threading
+import urllib
+from urllib import urlopen
+import json
+import time
+import os
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
+title_regex = re.compile(r'(?:https?:\/\/.*\/images?\/.*\/)(.*)', re.I)
+
+class TumblerGetter():
+
+    @staticmethod
+    def ReadCSVasDict(csv_file):
+        try:
+            with open(csv_file) as csvfile:
+                reader = csv.DictReader(csvfile)
+                final_result = []
+                items = []
+                for row in reader:
+                    item = {}
+                    item = {
+                            'title': row['title'],
+                            'start': row['start'],
+                            'end': row['end'],
+                            'now': row['now'],
+                            'image': row['image'],
+                            'file_name': row['file_name']
+                            }
+                    items.append(item)
+                final_result += items
+        except IOError as (errno, strerror):
+                print("I/O error({0}): {1}".format(errno, strerror))    
+        return final_result
+    @staticmethod        
+    def WriteDictToCSV(csv_file,csv_columns,dict_data):
+        try:
+            with open(csv_file, 'w') as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+                writer.writeheader()
+                for data in dict_data:
+                    writer.writerow(data)
+        except IOError as (errno, strerror):
+            print("I/O error({0}): {1}".format(errno, strerror))    
+        return   
+
+    @staticmethod
+    def BootStrap():
+            sock = urlopen("http://fuse-music.herokuapp.com/api/programs")
+            htmlSource = sock.read()
+            sock.close() 
+            url = htmlSource
+            artists = json.loads(url)
+            dict = artists.get('programs')
+            final_result = []
+            items = []
+            for artist in dict:
+                title = artist.get("title")
+                image = artist.get("image")
+                if not image:
+                    image = "https://applesocial.s3.amazonaws.com/assets/images/branding/on-air-default.jpg"
+                start = int(artist.get("start"))
+                time_now = int(time.time())
+                start = str(start)[:-3]
+                end = int(artist.get("end"))
+                end = str(end)[:-3]
+                if int(end) < int(time_now):
+                    #print('Skipping already aired: ' + title)
+                    continue
+                file_name = title_regex.search(image).group(1)
+                if not os.path.isfile(file_name):
+                    f = open(file_name,'wb')
+                    f.write(urllib.urlopen(image).read())
+                    f.close()
+                item = {}
+                item = {
+                        'title': title,
+                        'start': start,
+                        'end': end,
+                        'now': time_now,
+                        'image': image,
+                        'file_name': file_name
+                        }
+                items.append(item)
+            final_result += items
+            import csv
+            my_dict = final_result
+            csv_columns = ['file_name', 'title','start','end','now','image']
+            csv_file = "db.csv"
+
+            TumblerGetter.WriteDictToCSV(csv_file,csv_columns,my_dict)
 
 HOST_NAME = '0.0.0.0'
 PORT_NUMBER = 80
@@ -26,46 +116,17 @@ class SlowHandler(BaseHTTPRequestHandler):
     def do_GET(s):
         """Respond to a GET request."""
         if s.path.startswith('/audio/wat/showimg.jpg'):
-            import urllib
-            from urllib import urlopen
-            import json
-            import time
-            sock = urlopen("http://fuse-music.herokuapp.com/api/programs")
-            htmlSource = sock.read()
-            sock.close() 
-            url = htmlSource
-            artists = json.loads(url)
-            items = artists.get('programs')
-            result = []
-            for artist in items:
-                title = artist.get("title")
-                image = artist.get("image")
-                if not image:
-                    image = "http://applesocial.s3.amazonaws.com/assets/images/branding/on-air-default.jpg"
-                start = int(artist.get("start"))
-                time_now = int(time.time())
-                start = str(start)[:-3]
-                end = int(artist.get("end"))
-                end = str(end)[:-3]
-                if int(end) < int(time_now):
-                    #print('Skipping already aired: ' + title)
-                    continue
-                item = {
-                        'title': title,
-                        'start': start,
-                        'end': end,
-                        'now': time_now,
-                        'image': image}
-                result.append(item)
-                #print(item)
-            result.sort(key=lambda k: k['start'])
-            image = result[0]['image']
+            csv_file = "db.csv"
+            string = TumblerGetter.ReadCSVasDict(csv_file)
+            #print(string[0])
+            f=open(string[0]["file_name"], 'rb')
             s.send_response(200)
-            s.send_header('Content-type',        'image/jpg')
+            s.send_header('Content-type',        'image/png')
             s.end_headers()
-            image = urllib.urlopen(image).read()
-            s.wfile.write(image)
-        elif s.path.startswith('/audio/wat/show.jpg'):
+            s.wfile.write(f.read())
+            f.close()
+            
+        elif s.path.startswith('/audio/wat/show1.jpg'):
             #server = parse_qs(urlparse(s.path).query)
             img = Image.open("page.jpg")
             left = 460
@@ -162,13 +223,6 @@ class SlowHandler(BaseHTTPRequestHandler):
                 s.end_headers()
                 s.wfile.write(f.read())
                 f.close()
-        else:
-            f=open("page.jpg", 'rb')
-            s.send_response(404)
-            s.send_header('Content-type',        'image/jpg')
-            s.end_headers()
-            s.wfile.write(f.read())
-            f.close()
 
 
 def test(HandlerClass = SlowHandler,
@@ -180,6 +234,14 @@ if __name__ == '__main__':
     server_class = BaseHTTPServer.HTTPServer
     httpd = server_class((HOST_NAME, PORT_NUMBER), SlowHandler)
     print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
+    import threading
+    def UpdateJSON():
+        print("Updating JSON to CSV")
+        TumblerGetter.BootStrap()
+        threading.Timer(60, UpdateJSON).start()
+        print("Updated JSON to CSV")
+    UpdateJSON()
+    
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
